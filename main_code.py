166f03706e962c61fffa24e64f1c59b7301edacc
al0718 <122937282+al0718@users.noreply.github.com>
2023-03-28 11:34:20 -0400

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -1,9 +1,21 @@
 import pandas as pd
 import numpy as nm
-
-df = pd.read_csv('onlineRetail.csv', sep = ',', encoding='utf-8', encoding_errors='ignore')
-
+#Reads the csv with proper encoding. 
+df = pd.read_csv('onlineRetailGITHUB.csv', sep = ',', encoding='utf-8', encoding_errors='ignore')
+#Deletes rows in the original DataFrame with missing cells. 
 df.dropna(inplace = True)
+#Turns InvoiceNo column to a float column. 
+df['InvoiceNo'] = df['InvoiceNo'].str.replace('\D', '', regex=True)
+df['InvoiceNo'] = df['InvoiceNo'].astype(float)
+#Calculates the mean, median, variance, and standard deviation of the UnitPrice and InvoiceNo columns. 
+priceMean = df["UnitPrice"].mean()
+invoiceMean = df["InvoiceNo"].mean()
+priceMed = df["UnitPrice"].median()
 
-#print(df.head(10))
-print(df.to_string())
+
+print(df.head(10))
+#print(df.to_string())
+
+print(priceMean)
+print(int(invoiceMean))
+print(priceMed)
