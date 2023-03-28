759e4de3ff931714736ebead5421f0f23c02accd
al0718 <122937282+al0718@users.noreply.github.com>
2023-03-28 16:35:59 -0400

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -7,15 +7,29 @@ df.dropna(inplace = True)
 #Turns InvoiceNo column to a float column. 
 df['InvoiceNo'] = df['InvoiceNo'].str.replace('\D', '', regex=True)
 df['InvoiceNo'] = df['InvoiceNo'].astype(float)
-#Calculates the mean, median, variance, and standard deviation of the UnitPrice and InvoiceNo columns. 
+df['UnitPrice'] = df['UnitPrice'].astype(float)
+#Calculates the mean, median, mode, variance, and standard deviation of the UnitPrice and InvoiceNo columns. 
 priceMean = df["UnitPrice"].mean()
 invoiceMean = df["InvoiceNo"].mean()
 priceMed = df["UnitPrice"].median()
+invoiceMed = df["InvoiceNo"].mean()
+priceMode = df["UnitPrice"].mode()
+invoiceMode = df["InvoiceNo"].mode()
+priceVar = df["UnitPrice"].var()
+invoiceVar = df["InvoiceNo"].var()
+priceStand = df["UnitPrice"].std()
+invoiceStand = df["InvoiceNo"].std()
 
-
-print(df.head(10))
+#print(df.head(10))
 #print(df.to_string())
 
 print(priceMean)
 print(int(invoiceMean))
 print(priceMed)
+print(int(invoiceMed))
+print(priceMode)
+print(int(invoiceMode))
+print(priceVar)
+print(invoiceVar)
+print(priceStand)
+print(invoiceStand)
