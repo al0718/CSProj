1b8a8d5d1961cfb972b89feecf80743053e3d932
al0718 <122937282+al0718@users.noreply.github.com>
2023-04-04 09:13:03 -0400

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -31,15 +31,53 @@ print(classStand)
 #This prints all of the values in the DataFrame as a string, set up like a table. 
 print(df.to_string())
 '''
-#This will be used to make a pie chart. 
+#This function will go through each x and y value and add a label directly above each bar, plot, etc.
+def addlabels(x,y):
+    for i in range(len(x)):
+        #The ha = center formats it to the center of each mark. The font is adjusted using the fontdict. The other values relate to the printing and the centering of the values. 
+        plt.text(i, y[i]//2, y[i], ha = 'center', fontdict = value_font)
+
+#These values will save the various fonts used throughout the graphs. 
+title_font = {'family':'serif','weight':'bold','size':18 }
+small_font = {'family':'serif','weight':'bold','size':12 }
+value_font = {'family':'arial','weight':'bold','size':10 }
+
+#The following counts the amount of patients at each class of breast cancer and groups them by class. This will be used to make a pie chart. 
 df1 = df.groupby(['ClassNum'])['ClassNum'].count()
 print(df1)
-#This will be used to make a bar graph. 
+#The x_1 variable will create a Numpy array containing the different classes of breast cancer found in the pool of patients, or the x-values. 
+x_1 = ['Class 2','Class 4']
+#The y_1 variable will create a Numpy array contianing the amount of patients at each class of breast cancer, or the y-values. 
+y_1 = np.array([443, 239])
+#The following sets the bar graph to a specific size using the figure() function. 
+plt.figure(figsize = (10, 5))
+#This adds a title to the bar graph. 
+plt.title('Number of Patients per Class of Breast Cancer', fontdict = title_font)
+#This saves the pie chart as a variable so edits (like legends, axis titles, etc.) can be made. It also adds percentages to be displayed on each wedge.
+pie = plt.pie(y_1, labels = x_1, autopct='%.1f%%',
+    #This adjusts the style by adding a line between each wedge to make things more clear and by changing the font of the labels. 
+    wedgeprops = {'linewidth': 3.0, 'edgecolor': 'white'},
+    textprops = {'family':'serif','weight':'bold','size':12 })
+
+#The following groups and counts the amount of patients with each level of bland chromatin. This will be used to make a bar graph. 
 df2 = df.groupby(['BlandChromatin'])['BlandChromatin'].count()
 print(df2)
+#The x_2 variable will create a Numpy array containing each value for bland chromatin, or the x values. 
 x_2 = np.array(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
+#The y_2 variable will create a Numpy array containing the amount of patients with each level of bland chromatin, or the y values. 
 y_2 = np.array([150, 159, 161, 39, 34, 9, 71, 28, 11, 20])
-x_2_label = "Number of Patients"
-y_2_label = "Bland Chromatin Amount"
-plt.bar(x_2,y_2, labels = x_2_label)
+#The following sets the bar graph to a specific size using the figure() function. 
+plt.figure(figsize = (10, 5))
+#This saves the bar plot as a variable so edits (like legends, axis titles, etc.) can be made. The color of the bars on the graph is also determined here. 
+bar = plt.bar(x_2,y_2, color = 'orange')
+#A call is made to the addlabels fuction, to add labels for each bar. 
+addlabels(x_2, y_2)
+#This adds a title to the bar graph. 
+plt.title('Number of Patients with Each Level of Bland Chromatin', fontdict = title_font)
+#This adds a label on the x-axis. 
+plt.xlabel('Level of Bland Chromatin', fontdict = small_font)
+#This adds a label on the y-axis. 
+plt.ylabel('Number of Patients', fontdict = small_font)
+#This outputs our desired bar graph. 
 plt.show()
+
